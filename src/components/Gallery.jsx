31f8d280659baa0e2dd4515649034a21d7d6da92
Brandon Towers-Ferrett <brandon.toferrett@gmail.com>
2024-06-04 14:02:56 +0100

    should be responsive

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,7 +4,7 @@ const Gallery = () => {
   return (
     <div className='mt-16 text-center'>
       <h2 className='text-muted text-md mb-2'>WHAT WE DO</h2>
-      <h1 className='text-center text-4xl font-bold'>Check Out Our Work</h1>
+      <h1 className='text-center text-4xl font-bold mx-1'>Check Out Our Work</h1>
       <div className="container mx-auto px-5 py-2 lg:px-32 pt-16">
         <div className="-m-1 flex flex-wrap md:-m-2">
           <div className="flex w-full md:w-1/2 flex-wrap">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ import HeroImage from '../assets/ethernet.jpeg';
 const Hero = () => {
   return (
     <div className='relative'>
-      <div className="hero flex min-h-screen md:min-h-[70vh] justify-center items-center md:items-start">
-      <div className="custom-shape-divider-bottom-1717442289 border-base-200">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M649.97 0L599.91 54.12 550.03 0 0 0 0 120 1200 120 1200 0 649.97 0z" className='fill-base-200 z-[1]'></path>
-          </svg>
+      <div className="hero flex min-h-screen lg:min-h-[70vh] justify-center items-center lg:items-start">
+      <div class="custom-shape-divider-bottom-1717502932 z-[1] hidden lg:block">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="fill-base-200"></path>
+        </svg>
       </div>
         {/* Background image and dark overlay */}
         <div className="absolute inset-0 bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${HeroImage})` }}>
@@ -16,7 +16,7 @@ const Hero = () => {
         </div>
         <div className="w-full flex md:pt-8 flex-col items-center justify-center max-w-[80%] mx-auto relative z-10 text-center md:mt-0 -mt-20">
           <div className="text-center z-20 relative w-full mt-[88px]">
-            <h1 className="text-5xl lg:text-7xl font-bold max-w-3xl mx-auto hero-header !text-white">
+            <h1 className="text-5xl md:text-7xl font-bold max-w-3xl mx-auto hero-header !text-white">
               Hampshire Based Electrical Company
             </h1>
             <p className="py-6 max-w-2xl mx-auto text-lg !text-white">
@@ -33,9 +33,9 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="py-10 md:py-1 -mt-1 md:-mt-6 bg-base-200 shadow-lg cards">
-        <div className="flex flex-wrap justify-center items-center -mt-5 gap-y-10 big-z">
-          <div className="card sm:min-w-[300px] w-full sm:w-1/4 mx-10 rounded-none card">
+      <div className="py-10 md:py-1 md:-mt-1 bg-base-200 shadow-lg cards">
+        <div className="flex lg:flex-nowrap flex-wrap justify-center lg:ml-10 items-center gap-y-10 big-z">
+          <div className="card sm:min-w-[300px] w-full sm:w-1/4 mx-10 rounded-none md:pt-0 lg:-mt-7 card">
             <div className="card-body bg-transparent">
               <div className="flex gap-3 items-start">
                 <svg className="w-[30px] h-[30px] fill-base-content mt-[3px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
@@ -48,23 +48,23 @@ const Hero = () => {
               </div>
             </div>
           </div>
-          <div className="card sm:min-w-[300px] w-full sm:w-1/4 mx-10 rounded-none card">
-            <div className="card-body bg-transparent">
-              <div className="flex gap-3 items-start">
-              <svg className="w-[30px] h-[30px] fill-base-content mt-[3px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M349.4 44.6c5.9-13.7 1.5-29.7-10.6-38.5s-28.6-8-39.9 1.8l-256 224c-10 8.8-13.6 22.9-8.9 35.3S50.7 288 64 288H175.5L98.6 467.4c-5.9 13.7-1.5 29.7 10.6 38.5s28.6 8 39.9-1.8l256-224c10-8.8 13.6-22.9 8.9-35.3s-16.6-20.7-30-20.7H272.5L349.4 44.6z"/></svg>
-                <div>
-                  <h2 className="card-title mb-2 text-3xl">PAT Testing</h2>
-                  <p className="">If a dog chews shoes whose shoes does he choose?</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card sm:min-w-[300px] w-full sm:w-1/4 mx-10 rounded-none card">
+          <div className="card sm:min-w-[300px] w-full sm:w-1/4 mx-10 lg:-mt-16 rounded-none card">
             <div className="card-body bg-transparent">
               <div className="flex gap-3 items-start">
               <svg className="w-[30px] h-[30px] fill-base-content mt-[3px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z"/></svg>
                 <div>
                   <h2 className="card-title mb-2 text-3xl">Free Quotes</h2>
+                  <p className="">If a dog chews shoes whose shoes does he choose?</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card sm:min-w-[300px] w-full sm:w-1/4 mx-10 rounded-none lg:-mt-7 card">
+            <div className="card-body bg-transparent">
+              <div className="flex gap-3 items-start">
+              <svg className="w-[30px] h-[30px] fill-base-content mt-[3px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M349.4 44.6c5.9-13.7 1.5-29.7-10.6-38.5s-28.6-8-39.9 1.8l-256 224c-10 8.8-13.6 22.9-8.9 35.3S50.7 288 64 288H175.5L98.6 467.4c-5.9 13.7-1.5 29.7 10.6 38.5s28.6 8 39.9-1.8l256-224c10-8.8 13.6-22.9 8.9-35.3s-16.6-20.7-30-20.7H272.5L349.4 44.6z"/></svg>
+                <div>
+                  <h2 className="card-title mb-2 text-3xl">PAT Testing</h2>
                   <p className="">If a dog chews shoes whose shoes does he choose?</p>
                 </div>
               </div>
