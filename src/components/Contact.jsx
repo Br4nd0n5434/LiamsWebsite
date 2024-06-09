8c4526fe59db3393225d4c22278f3ed446a96593
Brandon <brandon.toferrett@gmail.com>
2024-06-09 23:39:04 +0100

    awdawdawd

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,13 +16,13 @@ import { BuildingOffice2Icon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/2
 
 export default function Example() {
   return (
-    <div className="relative isolate bg-white">
+    <div className="relative isolate bg-base-200">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
         <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-            <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
+            <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-base ring-1 ring-gray-900/10 lg:w-1/2">
               <svg
-                className="absolute inset-0 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+                className="absolute inset-0 h-full w-full stroke-base-content/40 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
                 aria-hidden="true"
               >
                 <defs>
@@ -37,15 +37,15 @@ export default function Example() {
                     <path d="M130 200V.5M.5 .5H200" fill="none" />
                   </pattern>
                 </defs>
-                <rect width="100%" height="100%" strokeWidth={0} fill="white" />
-                <svg x="100%" y={-1} className="overflow-visible fill-gray-50">
+                <rect width="100%" height="100%" strokeWidth={0} fill="black" className='fill-base-100' />
+                <svg x="100%" y={-1} className="overflow-visible fill-base-100">
                   <path d="M-470.5 0h201v201h-201Z" strokeWidth={0} />
                 </svg>
                 <rect width="100%" height="100%" strokeWidth={0} fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
               </svg>
             </div>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900">Get in touch</h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <h2 className=" font-bold tracking-tight text-3xl">Get in touch</h2>
+            <p className="mt-6 text-lg leading-8 text-base-content/40">
               Proin volutpat consequat porttitor cras nullam gravida at. Orci molestie a eu arcu. Sed ut tincidunt
               integer elementum id sem. Arcu sed malesuada et magna.
             </p>
@@ -53,33 +53,33 @@ export default function Example() {
               <div className="flex gap-x-4">
                 <dt className="flex-none">
                   <span className="sr-only">Address</span>
-                  <BuildingOffice2Icon className="h-7 w-6 text-gray-400" aria-hidden="true" />
+                  <BuildingOffice2Icon className="h-7 w-6 text-base-content" aria-hidden="true" />
                 </dt>
-                <dd>
-                  545 Mavis Island
+                <dd className='text-base-content'>
+                  114 The Crossway
                   <br />
-                  Chicago, IL 99191
+                  Portchester, Fareham
                 </dd>
               </div>
               <div className="flex gap-x-4">
                 <dt className="flex-none">
                   <span className="sr-only">Telephone</span>
-                  <PhoneIcon className="h-7 w-6 text-gray-400" aria-hidden="true" />
+                  <PhoneIcon className="h-7 w-6 text-base-content" aria-hidden="true" />
                 </dt>
                 <dd>
-                  <a className="hover:text-gray-900" href="tel:+1 (555) 234-5678">
-                    +1 (555) 234-5678
+                  <a className="text-base-content" href="tel:+1 (555) 234-5678">
+                    (+44) 7817175919
                   </a>
                 </dd>
               </div>
               <div className="flex gap-x-4">
                 <dt className="flex-none">
                   <span className="sr-only">Email</span>
-                  <EnvelopeIcon className="h-7 w-6 text-gray-400" aria-hidden="true" />
+                  <EnvelopeIcon className="h-7 w-6 text-base-content" aria-hidden="true" />
                 </dt>
                 <dd>
-                  <a className="hover:text-gray-900" href="mailto:hello@example.com">
-                    hello@example.com
+                  <a className="text-base-content" href="mailto:hello@example.com">
+                    office@ljelectricalservicesltd.co.uk
                   </a>
                 </dd>
               </div>
@@ -90,82 +90,88 @@ export default function Example() {
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
-                <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-base">
                   First name
                 </label>
                 <div className="mt-2.5">
                   <input
                     type="text"
                     name="first-name"
+                    required
                     id="first-name"
                     autoComplete="given-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-info sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div>
-                <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-base">
                   Last name
                 </label>
                 <div className="mt-2.5">
                   <input
                     type="text"
                     name="last-name"
+                    required
                     id="last-name"
                     autoComplete="family-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-info sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label htmlFor="email" className="block text-sm font-semibold leading-6 text-base">
                   Email
                 </label>
                 <div className="mt-2.5">
                   <input
                     type="email"
+                    required
                     name="email"
                     id="email"
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-info sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-base">
                   Phone number
                 </label>
                 <div className="mt-2.5">
                   <input
                     type="tel"
+                    required
                     name="phone-number"
                     id="phone-number"
                     autoComplete="tel"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-info sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label htmlFor="message" className="block text-sm font-semibold leading-6 text-base">
                   Message
                 </label>
                 <div className="mt-2.5">
                   <textarea
                     name="message"
+                    required
                     id="message"
                     rows={4}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-info sm:text-sm sm:leading-6"
                     defaultValue={''}
                   />
                 </div>
               </div>
             </div>
             <div className="mt-8 flex justify-end">
-              <button
-                type="submit"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+               <button
+                type='submit'
+                href="#"
+                className="rounded bg-logo-gradient px-9 py-4 text-sm font-semibold text-white shadow-xl hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Send message
+                Contact Us
               </button>
             </div>
           </div>
--- src/components/SocialPosts.jsx
+++ src/components/SocialPosts.jsx
@@ -20,19 +20,19 @@ const Card = ({ name, date, profileImage, content, likes, comments }) => {
   };
 
   return (
-    <div className="p-4 sm:p-6 md:p-8 lg:p-10 bg-gray-50 dark:bg-gray-900 flex items-center justify-center">
-      <div className="px-4 py-3 sm:px-6 sm:py-4 md:px-8 md:py-5 lg:px-10 lg:py-6 bg-white dark:bg-gray-800 shadow rounded-lg w-full max-w-md">
+    <div className="p-4 sm:p-6 md:p-8 lg:p-10 bg-base flex items-center justify-center">
+      <div className="px-4 py-3 sm:px-6 sm:py-4 md:px-8 md:py-5 lg:px-10 lg:py-6 bg-base-200 shadow rounded-lg w-full max-w-md">
         <div className="flex mb-3 items-center">
           {profileImage && <img src={profileImage} alt="Profile" className="w-12 h-12 sm:w-14 sm:h-14 rounded-full" />}
           <div className="ml-3">
-            <span className="block font-medium text-base sm:text-lg leading-tight text-black dark:text-gray-100">{name}</span>
-            <span className="block text-sm text-gray-500 dark:text-gray-400 font-light">{formatDate(date)}</span>
+            <span className="block font-medium text-base sm:text-lg leading-tight text-base-content">{name}</span>
+            <span className="block text-sm text-gray-500 font-light">{formatDate(date)}</span>
           </div>
         </div>
-        <p className="text-gray-800 dark:text-gray-100 leading-snug md:leading-normal">{content}</p>
+        <p className="text-base-content/50 leading-snug md:leading-normal">{content}</p>
         <div className="flex justify-between items-center mt-4">
           <div className="flex">
-            <svg className="p-0.5 h-6 w-6 sm:h-7 sm:w-7 rounded-full z-20 bg-white dark:bg-gray-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <svg className="p-0.5 h-6 w-6 sm:h-7 sm:w-7 rounded-full z-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
             <defs>
                   <linearGradient id="a1" x1="50%" x2="50%" y1="0%" y2="100%">
                     <stop offset="0%" stopColor="#18AFFF"/>
@@ -52,9 +52,9 @@ const Card = ({ name, date, profileImage, content, likes, comments }) => {
                   <path fill="white" d="M12.162 7.338c.176.123.338.245.338.674 0 .43-.229.604-.474.725a.73.73 0 01.089.546c-.077.344-.392.611-.672.69.121.194.159.385.015.62-.185.295-.346.407-1.058.407H7.5c-.988 0-1.5-.546-1.5-1V7.665c0-1.23 1.467-2.275 1.467-3.13L7.361 3.47c-.005-.065.008-.224.058-.27.08-.079.301-.2.635-.2.218 0 .363.041.534.123.581.277.732.978.732 1.542 0 .271-.414 1.083-.47 1.364 0 0 .867-.192 1.879-.199 1.061-.006 1.749.19 1.749.842 0 .261-.219.523-.316.666zM3.6 7h.8a.6.6 0 01.6.6v3.8a.6.6 0 01-.6.6h-.8a.6.6 0 01-.6-.6V7.6a.6.6 0 01.6-.6z"/>
                 </g>
             </svg>
-            <span className="ml-1 text-gray-500 dark:text-gray-400 font-light">{likes}</span>
+            <span className="ml-1 text-gray-500 font-light">{likes}</span>
           </div>
-          <div className="ml-1 text-gray-500 dark:text-gray-400 font-light">{comments} comments</div>
+          <div className="ml-1 text-gray-500 font-light">{comments} comments</div>
         </div>
       </div>
     </div>
@@ -139,7 +139,7 @@ const SocialPosts = () => {
   if (error) return <p>Error loading posts: {error.message}</p>;
 
   return (
-    <div className="p-4 sm:p-6 md:p-8 lg:p-10 bg-gray-50 dark:bg-gray-900 flex flex-wrap justify-center">
+    <div className="p-4 sm:p-6 md:p-8 lg:p-10 bg-base flex flex-wrap justify-center">
       {posts.map((post, index) => (
         <Card
           key={index}
